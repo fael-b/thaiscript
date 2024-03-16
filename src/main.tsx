f011a41b3e22a6d71788dc64c3764f0067a487fe
Fael BASSETTI <fael.bassetti@gmail.com>
2024-03-16 18:19:58 +0100

    init: react query client

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { appRouter } from "./config/router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./config/query-client";
 import { MantineProvider } from "@mantine/core";
 import { thaiScriptTheme } from "./config/mantine-theme";
 import "./styles.css";
@@ -10,7 +12,9 @@ import "@mantine/core/styles.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <MantineProvider theme={thaiScriptTheme}>
-      <RouterProvider router={appRouter} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={appRouter} />
+      </QueryClientProvider>
     </MantineProvider>
   </React.StrictMode>,
 );
