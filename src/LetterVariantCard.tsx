ea1a69f199ccc4b1d48b6e6165217ad77ceba712
Fael BASSETTI <fael.bassetti@gmail.com>
2024-03-17 12:23:23 +0100

    feat: use specific font for thai letters

--- src/LetterVariantCard.tsx
+++ src/LetterVariantCard.tsx
@@ -24,6 +24,7 @@ export function LetterVariantCard({
           c="thai-purple"
           style={{
             fontSize: 38,
+            fontFamily: "Sarabun, sans-serif",
           }}
         >
           {letter}
